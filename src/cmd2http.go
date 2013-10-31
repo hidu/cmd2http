998ee68558e94c8e41b8dde6dbb3c1128b4fe04c
duwei <duv123@gmail.com>
2013-10-31 23:10:53 +0800

    add version and sup static

--- src/cmd2http.go
+++ src/cmd2http.go
@@ -20,10 +20,12 @@ var configPath=flag.String("conf","./cmd2http.conf","config file")
 
 var port int
 
+const VERSION="20131031 1.0"
+
 type param struct{
   name string
   defaultValue string
-  isOption bool
+  isValParam bool
 }
 
 type Conf struct{
@@ -49,9 +51,13 @@ func main(){
     startHttpServer()
 }
 
+func (p *param)ToString() string{
+    return fmt.Sprintf("name:%s,default:%s,isValParam:%x",p.name,p.defaultValue,p.isValParam);
+}
 func loadConfig(){
    log.Println("start load conf [",*configPath,"]")
    var err error
+   log.Println("use conf:",*configPath)
    config, err= jsonConf.Load(*configPath)
 	if err != nil {
 	  log.Println(err.Error(),config)
@@ -74,18 +80,20 @@ func loadConfig(){
 	    }
 	   conf.cmdStr,_=_conf["cmd"].(string)
 	   conf.cmdStr=strings.TrimSpace(conf.cmdStr)
+	   conf.params=make([]*param,0,10)
 	   
 	   ps:=regexp.MustCompile(`\s+`).Split(conf.cmdStr,-1)
+//	   fmt.Println(ps)
 	   conf.cmd=ps[0]
 	   
 	   for i:=1;i<len(ps);i++ {
 	       item:=ps[i]
+//	       fmt.Println("i:",i,item)
 	        _param:=new(param)
 	        _param.name=item
-	       _param.isOption=true
 	       
 	       if(item[0]=='$'){
-	        _param.isOption=false;
+	        _param.isValParam=true;
 	        tmp:=strings.Split(item+"|","|")
 	        _param.name=tmp[0][1:]
 	        _param.defaultValue=tmp[1]
@@ -103,9 +111,10 @@ func loadConfig(){
 func startHttpServer(){
 //   http.ReadTimeout=60 * time.Second
    
+   http.Handle("/s/",http.FileServer(http.Dir("./")))
    http.HandleFunc("/",myHandler_root)
    http.HandleFunc("/help",myHandler_help)
-  
+   
    addr:=fmt.Sprintf(":%d",port)
    log.Println("listen at",addr)
    fmt.Println("listen at",addr)
@@ -129,9 +138,16 @@ func myHandler_root(w http.ResponseWriter, r *http.Request){
      startTime:=time.Now()
 	  path:=strings.Trim(r.URL.Path,"/")
 	  if(path==""){
+	       _,err := os.Stat( "./s/index.html" )
+			if err == nil {
+		     http.Redirect(w,r,"/s/",302)
+			  return;
+			}
 	      myHandler_help(w,r)
 	      return;
 	   }
+	   
+	   
 	  logStr:=r.RemoteAddr+" req:"+r.RequestURI
 	  defer func(){
 	       log.Println(logStr)
@@ -144,17 +160,17 @@ func myHandler_root(w http.ResponseWriter, r *http.Request){
 	     return;
 	  }
 	  
-	  args:=make([]string,len(conf.params))
+	  args:=make([]string,len(conf.params)+1)
 	  for i,_param:=range conf.params{
-		  if(_param.isOption){
-	        args[i]=_param.name
+		  if(!_param.isValParam){
+	        args[i+1]=_param.name
 		     continue
 		  }
 	     val:=r.FormValue(_param.name)
 	     if(val==""){
 	        val=_param.defaultValue
 	      }
-	      args[i]=val
+	      args[i+1]=val
 	  }
 	  cmd := Command(conf.cmd,args)
 	  var out bytes.Buffer
@@ -189,11 +205,12 @@ func myHandler_root(w http.ResponseWriter, r *http.Request){
 	   }
 }
 
+
 func myHandler_help(w http.ResponseWriter, r *http.Request){
    str:=`<!DOCTYPE html><html>
          <head>
          <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-         <title>{{.title}} cmd2http</title>
+         <title>{{.title}} cmd2http {{.version}}</title>
          <style>
 				.cpanel{background:#ffffff;border-radius:10px;margin-bottom: 10px;border:1px solid #e6eaed;}
 				.cpanel .hd{background:#e6eaed;padding: 3px 0 3px 10px;border-radius:10px 10px 0 0;color:#000;font-weight: bold;}
@@ -273,11 +290,13 @@ func myHandler_help(w http.ResponseWriter, r *http.Request){
 	   
 	   tpl,_:=template.New("page").Parse(str)
 	   values :=make(map[string]string)
+	   values["version"]=VERSION
 	   values["title"]=title
 	   values["option_cmd"]=option_cmd
 	   
 	   jsonByte,_:=json.Marshal(msgs)
 	   values["msgs"]=string(jsonByte)
 	   
+	   w.Header().Add("c2h",VERSION)
 	   tpl.Execute(w,values)
 }
